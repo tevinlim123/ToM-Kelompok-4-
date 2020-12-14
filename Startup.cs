5d044d4f3c18de39cb37a067d9531bb8d8905d05
tevinlim123 <YOU@EXAMPLE.COM>
2020-12-14 11:17:12 +0700

    ToM (Kelompok 4)

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ToM.Startup))]
+namespace ToM
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
